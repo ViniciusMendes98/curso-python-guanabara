099a368a0c83415f2f7b5dac48f3069566ad5edb
Vinicius <vinitu199809@gmail.com>
2026-08-30 19:04:21 -0300

    resolução até o exercio 35 - mundo 1

--- mundo-1-fundamentos/Condicoes-em-python-if-else/ex032.py
+++ mundo-1-fundamentos/Condicoes-em-python-if-else/ex032.py
@@ -1,5 +1,8 @@
 #Faça um programa que leia um ano qualquer e mostre se ele é bissexto.
 ano = int(input("Digite um ano qualquer: "))
+'''Para pegar o ano atual, você pode usar a biblioteca datetime do Python:
+ import datetime
+ ano_atual = datetime.datetime.now().year'''
 if (ano % 4 == 0 and ano % 100 != 0) or (ano % 400 == 0):
     print(f'O ano {ano} é bissexto.')
 else:

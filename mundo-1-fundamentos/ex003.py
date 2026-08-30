85203e795408caa915e28ac4f16966c303f07a53
Vinicius <vinitu199809@gmail.com>
2026-08-30 06:01:30 -0300

    adicionei os exercicios 3 e 4 do mundo 1

--- mundo-1-fundamentos/ex003.py
+++
@@ -1,6 +0,0 @@
-#Crie um programa que leia dois numeros e mostre a soma entre eles.
-n1 = int(input('Digite um número: '))
-n2 = int(input('Digite outro número: '))
-soma = n1 + n2
-print('A soma entre {} e {} é igual a {}'.format(n1, n2, soma))
-#outra solução é: print(f'A soma entre {n1} e {n2} é igual a {soma}')

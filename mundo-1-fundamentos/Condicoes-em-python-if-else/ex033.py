6e5fd1df2a9a764a767b0c69e519f06726ee1a1a
Vinicius <vinitu199809@gmail.com>
2026-08-31 22:33:07 -0300

    Consertei alguns inputs e alguns prints e resolvi ate o exercio 35 - mundo 1, concluindo o mundo 1 de Python

--- mundo-1-fundamentos/Condicoes-em-python-if-else/ex033.py
+++ mundo-1-fundamentos/Condicoes-em-python-if-else/ex033.py
@@ -2,18 +2,6 @@
 n1 = int(input('Digite o primeiro número: '))
 n2 = int(input('Digite o segundo número: '))
 n3 = int(input('Digite o terceiro número: '))
-if n1 >= n2 and n1 >= n3:
-    maior = n1
-elif n2 >= n1 and n2 >= n3:
-    maior = n2
-else:
-    maior = n3
-
-if n1 <= n2 and n1 <= n3:
-    menor = n1
-elif n2 <= n1 and n2 <= n3:
-    menor = n2
-else:
-    menor = n3
-
+maior = max(n1, n2, n3)
+menor = min(n1, n2, n3)
 print(f'O maior número é {maior} e o menor número é {menor}.')
--- mundo-1-fundamentos/Condicoes-em-python-if-else/ex034.py
+++ mundo-1-fundamentos/Condicoes-em-python-if-else/ex034.py
@@ -2,7 +2,6 @@
 # Para salários superiores a R$ 1250,00, calcule um aumento de 10%.
 # Para os inferiores ou iguais, o aumento é de 15%.
 salario = float(input('Digite o salário do funcionário: R$ '))
-
 if salario > 1250:
     novo_salario = salario + (salario * 0.10)
     print(f'O novo salário será de R$ {novo_salario:.2f}')

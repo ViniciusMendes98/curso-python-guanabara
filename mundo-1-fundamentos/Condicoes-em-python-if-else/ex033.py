5444260d41d0bbd0eeeb9621d04831c316c3859d
Vinicius <vinitu199809@gmail.com>
2026-08-31 23:32:24 -0300

    coloquei outra forma de resolver o ex033

--- mundo-1-fundamentos/Condicoes-em-python-if-else/ex033.py
+++ mundo-1-fundamentos/Condicoes-em-python-if-else/ex033.py
@@ -2,6 +2,17 @@
 n1 = int(input('Digite o primeiro número: '))
 n2 = int(input('Digite o segundo número: '))
 n3 = int(input('Digite o terceiro número: '))
-maior = max(n1, n2, n3)
-menor = min(n1, n2, n3)
+maior = max(n1, n2, n3)   #maximo entre os três números
+menor = min(n1, n2, n3)   #mínimo entre os três números
+'''Outra forma de fazer sem usar max e min:
+maior = n1
+menor = n1
+if n2 > maior:
+    maior = n2
+if n3 > maior:
+    maior = n3
+if n2 < menor:
+    menor = n2
+if n3 < menor:
+    menor = n3'''
 print(f'O maior número é {maior} e o menor número é {menor}.')
